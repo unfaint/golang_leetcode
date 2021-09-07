2f8954259bdfdf277165ec80ab808fd20d07a27d
Vladimir Kruglov <vladimir.kruglov@gmail.com>
2021-09-07 11:37:50 +0300

    o(n) solution

--- maximum_subarray_53/solution.go
+++ maximum_subarray_53/solution.go
@@ -1,5 +1,20 @@
 package maximumsubarray53
 
 func maxSubArray(nums []int) int {
-	return len(nums)
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	curSum, maxSum := nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > curSum {
+			curSum = nums[i]
+		} else {
+			curSum += nums[i]
+		}
+		if maxSum < curSum {
+			maxSum = curSum
+		}
+	}
+
+	return maxSum
 }
